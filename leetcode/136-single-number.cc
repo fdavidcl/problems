a42412ecfb7d570889976f71c26994f6f62463b8
David Charte <fdavidcl@protonmail.com>
2016-12-04 18:52:23 +0100

    [leet] Single number
    oneliner lol

---
+++ leetcode/136-single-number.cc
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/single-number/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        return accumulate(nums.begin(), nums.end(), 0, [](int acc, int e) { return acc ^ e; });
+    }
+};
